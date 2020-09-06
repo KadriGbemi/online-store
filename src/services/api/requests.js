30c1d7deaa125d8eb191819d81f98e1e53ebaa3a
kadri <kge.sportcompass.dk>
2020-09-06 12:06:14 +0300

    Added api methods and used axios for making requests

--- src/services/api/requests.js
+++ src/services/api/requests.js
@@ -1,19 +1,31 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = "https://fakestoreapi.com/products";
+axios.defaults.baseURL = 'https://fakestoreapi.com/products/';
 
 export function getAllProducts() {
   return axios({
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json',
     },
   });
 }
 
-// export function getAllProducts() {
-//     return axios({
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//     });
-//   }
+export function updateProduct(productId) {
+  return axios({
+    method: 'PUT',
+    url: productId,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+export function deleteProduct(productId) {
+    return axios({
+      method: 'DELETE',
+      url: productId,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
